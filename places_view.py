baad3d7d2329a06a3dc57c6629c9fbdcb8aa9d1e
210041125_shahrinhossain <shahrin@iut-dhaka.edu>
2024-04-26 22:45:25 +0600

    commit6

--- places_view.py
+++ places_view.py
@@ -9,8 +9,9 @@ places_view = Blueprint("places_view", __name__, static_folder="static", templat
 
 @places_view.route('/<string:name>/<string:city>/<string:country>/<float:lat>/<float:lon>/<string:type>', methods=['GET'])
 def view_place(name, city, country, lat, lon, type):
+    print('a')
     a_quality = ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor']
-    data = request.json
+    # data = request.json
     # var = {
     #     'name': data.get('name'),
     #     'lat': data.get('lat'),
@@ -19,6 +20,7 @@ def view_place(name, city, country, lat, lon, type):
     #     'country': data.get('country'),
     #     'type': data.get('type')
     # }
+    print('b')
     var = {
         'name': name,
         'lat': lat,
@@ -27,6 +29,7 @@ def view_place(name, city, country, lat, lon, type):
         'country': country,
         'type': type
     }
+    print('c')
     # var['lat'] = data.get('lat')
     # var['lon'] = data.get('lon')
     # name = data.get('name')
@@ -39,6 +42,7 @@ def view_place(name, city, country, lat, lon, type):
     # base_url = url_for(f"weather.get_weather({st_lat}, {st_lon})", _external=True)
     # base_url = url_for(f"weather.get_weather", _external=True)
     # endpoint2_url = f"{base_url}/{st_lat}/{st_lon}"
+    print('a')
     base_url = url_for('weather.get_weather', lat=st_lat, lon=st_lon, _external=True)
 
     response = requests.get(base_url)
@@ -51,3 +55,4 @@ def view_place(name, city, country, lat, lon, type):
         var['aqi'] = weather_data.get('aqi')
         var['weather_status'] = weather_data.get('weather_status')
         return render_template('location-details.html', var=var)
+
